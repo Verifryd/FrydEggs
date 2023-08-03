85854bf6457d046ba5ce1205ae8203c77e3c9f56
Verifryd <verifryd.video@gmail.com>
2023-08-03 05:12:20 -0400

    hmm

--- Eggsperience/src/script.js
+++ Eggsperience/src/script.js
@@ -1,5 +1,5 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from 'https://threejs.org/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'

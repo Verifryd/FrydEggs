c9b55bdc9438ae561a9f80a46ce7769c99d06add
Verifryd <verifryd.video@gmail.com>
2023-08-03 14:07:16 -0400

    kfjn

--- script.js
+++ script.js
@@ -1,94 +1,65 @@
+// Variables for Fabergé Egg
 var fabergeButton = document.getElementById('purchaseButton');
 var fabergeVideo = document.getElementById('productVideo');
+
+// Variables for Kringle Kronic
 var kringleButton = document.getElementById('kringleproductButton');
 var kringleVideo = document.getElementById('kringleproductVideo');
+
+// Variables for Don't Press the Egg
 var dpteButton = document.getElementById('dptepurchaseButton');
 var dpteVideo = document.getElementById('dpteproductVideo');
 
 if (fabergeButton && fabergeVideo) {
     fabergeButton.addEventListener('click', function() {
-        // Hide the product image and show the video player
         document.getElementById('fabergeImage').style.display = 'none';
         fabergeVideo.style.display = 'block';
         fabergeVideo.play();
-
         this.style.display = 'none';
 
-        // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
-        // and hide the video
         fabergeVideo.addEventListener('ended', function() {
             document.getElementById('fabergeImage').src = 'assets/EarlyEggcessA.gif';
             document.getElementById('fabergeImage').style.display = 'block';
             fabergeVideo.style.display = 'none';
+            document.getElementById('nextButton').disabled = false;
+            localStorage.setItem('fabergeProductViewed', 'true');
         });
-    });
-
-    fabergeVideo.addEventListener('ended', function() {
-        // Enable the "Next Egg" button when the video has ended
-        document.getElementById('nextButton').disabled = false;
-
     });
 }
 
 if (kringleButton && kringleVideo) {
     kringleButton.addEventListener('click', function() {
-        // Hide the product image and show the video player
         document.getElementById('kringleImage').style.display = 'none';
         kringleVideo.style.display = 'block';
         kringleVideo.play();
-
         this.style.display = 'none';
 
-        // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
-        // and hide the video
         kringleVideo.addEventListener('ended', function() {
             document.getElementById('kringleImage').src = 'assets/EarlyEggcessB.gif';
             document.getElementById('kringleImage').style.display = 'block';
             kringleVideo.style.display = 'none';
+            document.getElementById('nextButton').disabled = false;
+            localStorage.setItem('kringleProductViewed', 'true');
         });
-    });
-
-    kringleVideo.addEventListener('ended', function() {
-        // Enable the "Next Egg" button when the video has ended
-        document.getElementById('nextButton').disabled = false;
     });
 }
 
 if (dpteButton && dpteVideo) {
     dpteButton.addEventListener('click', function() {
-        // Hide the product image and show the video player
         document.getElementById('DPtEImage').style.display = 'none';
         dpteVideo.style.display = 'block';
         dpteVideo.play();
-
         this.style.display = 'none';
 
-        // When the video ends, change the Fabergé Egg image to the EarlyEggcessA GIF
-        // and hide the video
         dpteVideo.addEventListener('ended', function() {
             document.getElementById('DPtEImage').src = 'assets/EarlyEggcessC.gif';
             document.getElementById('DPtEImage').style.display = 'block';
             dpteVideo.style.display = 'none';
+            document.getElementById('nextButton').disabled = false;
+            localStorage.setItem('DPtEProductViewed', 'true');
         });
     });
-
-    dpteVideo.addEventListener('ended', function() {
-        // Enable the "Next Egg" button when the video has ended
-        document.getElementById('nextButton').disabled = false;
-    });
 }
-
-// When the video ends, enable the "Next Egg" button and set a flag in localStorage
-video.addEventListener('ended', function() {
-    document.getElementById('nextButton').disabled = false;
-    localStorage.setItem('kringleProductViewed', 'true'); // Use a different key for each product
-});
-
-// When the video ends, enable the "Next Egg" button and set a flag in localStorage
-video.addEventListener('ended', function() {
-    document.getElementById('nextButton').disabled = false;
-    localStorage.setItem('DPtEProductViewed', 'true'); // Use a different key for each product
-});
 
 window.onload = function() {
     // If the 'kringleProductViewed' flag is not set in localStorage, disable the link
@@ -97,23 +68,11 @@ window.onload = function() {
             event.preventDefault();
         }
     }
-    // Repeat for each product...
-};
 
-window.onload = function() {
     // If the 'DPtEProductViewed' flag is not set in localStorage, disable the link
     if (!localStorage.getItem('DPtEProductViewed')) {
         document.getElementById('DPtELink').onclick = function(event) {
             event.preventDefault();
         }
     }
-    // Repeat for each product...
 };
-
-
-window.onload = function() {
-    // your code here
-};
-
-
-
